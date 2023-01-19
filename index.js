697e1f382559e8e7bc46bd1b1669d21ec00eb757
HenryBri <henrybrindfeldt@gmail.com>
2023-01-19 17:46:49 +0200

    Add dotenv and .env file to store environment variables [#184267779]

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require("dotenv").config()
 const app = require('express')()
-const port = 8080
+const port = process.env.APP_PORT
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
