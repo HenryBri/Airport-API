a8e28b768259f1b1fde9d6f376caaf28c0d0e6cc
HenryBri <henrybrindfeldt@gmail.com>
2023-02-27 20:11:18 +0200

    Update 184268194

--- controllers/AirportFlightsController.js
+++
@@ -1,9 +0,0 @@
-const { db } = require("../db")
-const FilmUsers = db.airportFlights
-const Film = db.airports
-const User = db.flights
-const QueryTypes = db.Sequelize.QueryTypes
-
-exports.getAll = async (req, res) => {
-    
-}
